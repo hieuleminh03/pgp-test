4fc7765b5c911e926c39362b1aa433603c95db0f
Yian <hieulm.data@gmail.com>
2024-04-24 23:05:14 +0700

    feat: implement encryptUint8Array fix: genKey(data)

--- genKey.js
+++ genKey.js
@@ -1,11 +1,12 @@
 const openpgp = require('openpgp');
 
-const data = {
-    userIDs: [{ name: 'Jon Smith', email: 'ok@test.com' }],
-    passphrase: 'super long and hard to guess secret',
-};
 
-async function generateKey(data) {
+
+async function generateKey() {
+    const data = {
+        userIDs: [{ name: 'Jon Smith', email: 'ok@test.com' }],
+        passphrase: 'super long and hard to guess secret',
+    };
     const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
         type: data.type || 'ecc',
         curve: data.curve || 'curve25519',
@@ -16,7 +17,7 @@ async function generateKey(data) {
     return { privateKey, publicKey, revocationCertificate };
 }
 
-generateKey(data).then((keys) => {
+generateKey().then((keys) => {
     for (const key in keys) {
         console.log(key + ':\n\n', keys[key]);
         console.log('--------------------------\n');
